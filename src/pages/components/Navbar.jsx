1523df839fe2a5204c07d8360ced4d8220bd286c
aileen517 <legarde01@gmail.com>
2022-11-01 21:59:18 +0800

    first commit

--- src/pages/components/Navbar.jsx
+++ src/pages/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-primary">
+    <nav className="navbar navbar-expand-lg navbar-light bg-secondary">
       <div className="container-fluid">
         <Link className="navbar-brand" href="#">
           Navbar
